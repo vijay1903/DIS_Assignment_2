889c8e49d60f757e73f7029b414ef7e072e130dd
Yaassh Rao <yaasshrao@Yaasshs-MacBook-Air.local>
2018-10-02 00:40:23 -0400

    Student3

--- Assignment_2/StockList_Students_3.cs
+++ Assignment_2/StockList_Students_3.cs
@@ -6,40 +6,64 @@ namespace Assignment_2
 {
   public partial class StockList
   {
-    //param        : NA
-    //summary      : Calculate the value of each node by multiplying holdings with price, and returns the total value
-    //return       : total value
-    //return type  : decimal
-    public decimal Value()
-    {
-      decimal value = 0.0m;
+        //param        : NA
+        //summary      : Calculate the value of each node by multiplying holdings with price, and returns the total value
+        //return       : total value
+        //return type  : decimal
 
-      // write your implementation here
+        public decimal Value()
+        {
+            decimal value = 0.0m;
 
-      return value;
+            // write your implementation here
+            StockNode current = head;
+            while (current != null)
+            {
+                value += (current.StockHolding.Holdings * current.StockHolding.CurrentPrice);
+                current = current.Next;
+            }
+
+            return value;
+        }
+
+        public int Similarity(StockList listToCompare)
+        {
+            int similarityIndex = 0;
+            // Considering Similarity means the Stocks with the same name.
+            StockNode node1 = head;
+            StockNode node2 = listToCompare.head;
+            while(node1 != null)
+            {
+                while(node2 != null)
+                {
+                    if(node1.StockHolding.Name == node2.StockHolding.Name)
+                    {
+                        similarityIndex++;
+                    }
+                    node2 = node2.Next;
+                }
+                node1 = node1.Next;
+            }
+
+            return similarityIndex;
+        }
+
+        //param        : NA
+        //summary      : Print all the nodes present in the list
+        //return       : NA
+        //return type  : NA
+        public void Print()
+        {
+            StockNode current = head;
+            if (current == null)
+            {
+                Console.WriteLine("No Data to print");
+            }
+            while (current != null)
+            {
+                Console.WriteLine(current.StockHolding);
+                current = current.Next;
+            }
+        }
     }
-
-    //param  (StockList) listToCompare     : StockList which has to comared for similarity index
-    //summary      : finds the similar number of nodes between two lists
-    //return       : similarty index
-    //return type  : int
-    public int Similarity(StockList listToCompare)
-    {
-      int similarityIndex = 0;
-
-      // write your implementation here
-
-      return similarityIndex;
-    }
-
-    //param        : NA
-    //summary      : Print all the nodes present in the list
-    //return       : NA
-    //return type  : NA
-    public void Print()
-    {
-      // write your implementation here
-
-    }
-  }
 }
