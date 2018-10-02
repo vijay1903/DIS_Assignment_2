182497f20744d9e321db159827a3933325a7f302
tanay27 <tanaykarmarkar@gmail.com>
2018-10-02 00:12:44 -0400

    Student 2

--- Assignment_2/StockList_Students_2.cs
+++ Assignment_2/StockList_Students_2.cs
@@ -6,43 +6,64 @@ namespace Assignment_2
 {
   public partial class StockList
   {
-    //param   (StockList)listToMerge : second list to be merged 
-    //summary      : merge two different list into a single result list
-    //return       : merged list
-    //return type  : StockList
+        //param   (StockList)listToMerge : second list to be merged 
+        //summary      : merge two different list into a single result list
+        //return       : merged list
+        //return type  : StockList
     public StockList MergeList(StockList listToMerge)
     {
-      StockList resultList = new StockList();
+        StockList resultList = new StockList();
+        StockNode current = head;
+        while (current != null)
+        {
+            resultList.AddStock(current.StockHolding);
+            current = current.Next;
+        }
+        current = listToMerge.head;
+        while (current != null)
+        {
+            resultList.AddStock(current.StockHolding);
 
-      // write your implementation here
+            current = current.Next;
+        }
+        // write your implementation here
 
-      return resultList;
+        return resultList;
+    }
+        public Stock MostShares()
+        {
+        Stock mostShareStock = null;
+        mostShareStock = head.StockHolding;
+        StockNode current = head;
+        while (current != null)
+        {
+            if (mostShareStock.Holdings < current.StockHolding.Holdings)
+            {
+                mostShareStock = current.StockHolding;
+            }
+            current = current.Next;
+        }
+
+        return mostShareStock;
     }
 
-    //param        : NA
-    //summary      : finds the stock with most number of holdings
-    //return       : stock with most shares
-    //return type  : Stock
-    public Stock MostShares()
+        //param        : NA
+        //summary      : finds the number of nodes present in the list
+        //return       : length of list
+        //return type  : int
+        public int Length()
     {
-      Stock mostShareStock = null;
+        int length = 0;
 
-      // write your implementation here
+        StockNode current = head;
+        while (current != null)
+        {
+            length++;
+            current = current.Next;
+        }
 
-      return mostShareStock;
-    }
-
-    //param        : NA
-    //summary      : finds the number of nodes present in the list
-    //return       : length of list
-    //return type  : int
-    public int Length()
-    {
-      int length = 0;
-
-      // write your implementation here
-
-      return length;
+        return length;
+        }
     }
   }
 }
